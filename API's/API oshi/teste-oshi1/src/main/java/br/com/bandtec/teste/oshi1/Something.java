14e3d313b73b9c48f7598e8f8d423ddced14943f
marcelowhitehead <marcelo.cacace@bandtec.com.br>
2020-10-07 23:14:20 -0300

    hld lld v1.1

--- API's/API oshi/teste-oshi1/src/main/java/br/com/bandtec/teste/oshi1/Something.java
+++ API's/API oshi/teste-oshi1/src/main/java/br/com/bandtec/teste/oshi1/Something.java
@@ -1,9 +1,11 @@
 
 package br.com.bandtec.teste.oshi1;
 
+
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.HardwareAbstractionLayer;
+import oshi.hardware.GlobalMemory;
 
 public class Something {
     
@@ -11,10 +13,12 @@ public class Something {
             SystemInfo si = new SystemInfo();
             HardwareAbstractionLayer hal = si.getHardware();
             CentralProcessor cpu = hal.getProcessor();
+            GlobalMemory gm = h.getMemory();
             
             System.out.println(si);
             System.out.println(hal);
             System.out.println(cpu);
+            System.out.println(gm);
             
     }    
 }
