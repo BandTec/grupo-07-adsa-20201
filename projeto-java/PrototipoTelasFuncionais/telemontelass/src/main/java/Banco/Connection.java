616651eb90992f2fe81133092a0fb4c2514ee962
GabrielHLazaro <gabriel.lazaro@bandtec.com.br>
2020-11-24 17:16:51 -0300

    resolvendo conflitos

--- projeto-java/PrototipoTelasFuncionais/telemontelass/src/main/java/Banco/Connection.java
+++ projeto-java/PrototipoTelasFuncionais/telemontelass/src/main/java/Banco/Connection.java
@@ -18,7 +18,7 @@ public class Connection {
     public Connection() {
         this.datasource = new BasicDataSource();
         this.datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        this.datasource.setUrl("jdbc:mysql://localhost:3306/bdTelemon");
+        this.datasource.setUrl("jdbc:mysql://3.83.153.177:3306/bdTelemon");
         this.datasource.setUsername("root");
         this.datasource.setPassword("urubu100");
 
