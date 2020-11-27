9c365e58ead5a78e5e7e2c1fdebb39fa2166a9f2
MarkzBandtec <vitor.portela@bandtec.com.br>
2020-11-27 19:40:43 -0300

    JAR para ec2 [resolvido]

--- projeto-java/PrototipoTelasFuncionais/telemontelass/src/main/java/Banco/Insertbd.java
+++ projeto-java/PrototipoTelasFuncionais/telemontelass/src/main/java/Banco/Insertbd.java
@@ -29,16 +29,16 @@ public class Insertbd {
     }
 
     public void InserirProcessos(String name, double usoCpu, double usoMem) {
-
-        if (contador++ < 100) {
-            template.update("INSERT INTO tbProcessos VALUES (?,?,?,?,?,?)", null, name, usoCpu, usoMem, reg.dataFormatada + reg.horaFormatada, 1);
-
-            List consulta = template.queryForList("SELECT * FROM tbProcessos");
-        }else{
-            contador = 0;
+//
+//        if (contador++ < 100) {
+//            template.update("INSERT INTO tbProcessos VALUES (?,?,?,?,?,?)", null, name, usoCpu, usoMem, reg.dataFormatada + reg.horaFormatada, 1);
+//
+//            List consulta = template.queryForList("SELECT * FROM tbProcessos");
+//        }else{
+//            contador = 0;
             template.update("TRUNCATE TABLE tbProcessos;");
             template.update("INSERT INTO tbProcessos VALUES (?,?,?,?,?,?)", null, name, usoCpu, usoMem, reg.dataFormatada + reg.horaFormatada, 1);
-        }
+//        }
     }
 
     public void InserirComponente(String tipo, String desc) {
