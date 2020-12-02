c1ed6025b496ff6e1c67704db53ad5aaee26b4dd
cervalhub <gabriel.meira@bandtec.com.br>
2020-12-01 21:03:32 -0300

    Tentativa azure 5

--- projeto-dashboard/index.js
+++ projeto-dashboard/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname, './cliente', 'index.html'));
+    res.sendFile(path.join(__dirname, '/cliente', 'index.html'));
 });
 
 app.post('/user',(req,res)=>{
